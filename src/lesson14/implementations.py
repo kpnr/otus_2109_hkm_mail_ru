3076d15ea907687c0ac803e51d74d8433eb88b34
pervushin_dg <pervushin_dg@magnit.ru>
2021-11-20 20:10:26 +0300

    Theads-related IoCs

--- src/lesson14/implementations.py
+++ src/lesson14/implementations.py
@@ -1,12 +1,8 @@
 """Реализация IOC"""
 from typing import Any, Callable
+from threading import RLock, local
 from .interfaces import IoCController
 
-
-# class DeferCallCommand(GenericCommand):
-#     """Превращаем произвольный вызов в команду"""
-#
-#     def __init__(self, receiver:Tuple(Callable, List, Dict)):
 
 class SimpleIoC(IoCController):
     """
@@ -46,4 +42,37 @@ class PolymorphIoC(IoCController):
     def resolve(self, key: str, *args, **kwargs) -> Any:
         """Разрешение символа через внешний IoC"""
         rv = self.ioc.resolve(key, *args, **kwargs)
+        return rv
+
+
+class ThreadSafeIoC(SimpleIoC):
+    """Потокобезопасный (с межпоточной блокировкой)"""
+    def __init__(self):
+        super().__init__()
+        self.locker = RLock()
+
+    def resolve(self, key: str, *args, **kwargs) -> Any:
+        """Потокобезопасное разрешение зависимостей"""
+        with self.locker:
+            rv = super().resolve(key, *args, **kwargs)
+        return rv
+
+
+class ThreadLocalIoC(IoCController):
+    """
+    С разделением по потокам - каждый поток имеет свои зависимости,
+    т.е. ключом служит пара (Поток, Имя_Зависимости)
+    """
+
+    def __init__(self):
+        self.symbols = local()
+
+    def _register(self, key: str, value: Callable) -> Any:
+        self.symbols.__dict__[key] = value
+
+    def resolve(self, key: str, *args, **kwargs) -> Any:
+        """Локальный для потока IoC"""
+        # В новом потоке список зависимостей пуст, поэтому нужно зарегистрировать регистратора
+        self.symbols.__dict__.setdefault('ioc.register', self._register)
+        rv = self.symbols.__dict__[key](*args, **kwargs)
         return rv
--- test/lesson14_test.py
+++ test/lesson14_test.py
@@ -1,7 +1,11 @@
 """Тесты для урока № 14"""
 from typing import Any
+from time import sleep
+from random import random
+from threading import Thread
 from pytest import raises
-from lesson14.implementations import SimpleIoC, IoCController, PolymorphIoC
+from lesson14.implementations import (
+    SimpleIoC, IoCController, PolymorphIoC, ThreadSafeIoC, ThreadLocalIoC)
 
 
 def test_simple_ioc():
@@ -33,3 +37,52 @@ def test_polymorph_ioc():
         ioc.resolve('test')
     with raises(NotImplementedError):
         ioc.resolve('ioc.replace', DummyIoC())
+
+
+def test_thread_safe_ioc():
+    def SlowMethod() -> None:
+        nonlocal shared_value
+        local_value = shared_value
+        sleep(0.1 + random())  # сделаем паузу между чтением и записью
+        shared_value = local_value + 1
+
+    def thread_runner():
+        """Выполняется в отдельном потоке"""
+        ioc.resolve('test')
+
+    shared_value = 0
+    thread_count = 10
+    ioc = ThreadSafeIoC()
+    ioc.resolve('ioc.register', 'test', SlowMethod)
+    threads = [Thread(target=thread_runner) for _ in range(thread_count)]
+    for x in threads:  # Устроим гонки вокруг разделяемой переменной
+        x.start()
+    for x in threads:  # Подождем всех участников
+        x.join()
+    # Проверим, что гонки не привели к повреждению ресурса
+    assert shared_value == thread_count
+
+
+def test_thread_local_ioc():
+    def thread_runner(uniq_arg: int):
+        """Выполняется в отдельном потоке"""
+        nonlocal is_ok
+        ioc.resolve('ioc.register', 'test', lambda : uniq_arg * 2)
+        # Пауза
+        sleep(random())
+        # И проверка, что наша зависимость не испорчена другим потоком
+        processed_arg = ioc.resolve('test')
+        if processed_arg != uniq_arg * 2:
+            is_ok = False
+
+    ioc = ThreadLocalIoC()
+    thread_count = 10
+    is_ok = True
+    threads = [Thread(target=thread_runner, args=[x])
+               for x in range(thread_count)]
+    for x in threads:  # Устроим гонки вокруг общего IoC
+        x.start()
+    for x in threads:  # Подождем всех участников
+        x.join()
+    assert is_ok
+
