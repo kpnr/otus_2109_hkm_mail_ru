1350fdd55ff1df91ce2714f96e7ac1082368b02e
pervushin_dg <pervushin_dg@magnit.ru>
2021-12-12 19:09:49 +0300

    Исправление "зависания" теста из-за незавершенного Аттрактора

--- test/lesson23_test.py
+++ test/lesson23_test.py
@@ -2,7 +2,8 @@
 
 import pytest
 
-from queue import Empty
+from queue import Empty, Queue
+from threading import Thread
 from lesson23.implementations import (
     StatefulAttractor, StopAttractorCommand, NoOpCommand, MoveToCommand,
     RunCommand)
@@ -20,9 +21,11 @@ def test_hard_stop():
 
 
 def test_move_to_command():
-    """Проверка состояния MoveTo"""
+    """Проверка состояния MoveTo и переключние обратно в Run"""
     attractor = StatefulAttractor()
+    assert isinstance(attractor, Thread)
     command_queue = ThreadedQueue()
+    assert isinstance(command_queue, Queue)
     command_queue_2 = ThreadedQueue()
     attractor.start(command_queue)
     move_to_command = MoveToCommand((attractor, command_queue_2))
@@ -31,25 +34,6 @@ def test_move_to_command():
     command_queue.write(noop_command)
 
     # проверим, что noop_command была ретранслирована во вторую очередь
-    assert command_queue_2.get(True, 1.0) is noop_command
-
-
-def test_run_command():
-    """Проверка возврата к обычному режиму выполнения команд"""
-    # аттрактор
-    attractor = StatefulAttractor()
-    command_queue = ThreadedQueue()
-    attractor.start(command_queue)
-
-    command_queue_2 = ThreadedQueue()
-
-    # переключение в режим MoveTo
-    move_to_command = MoveToCommand((attractor, command_queue_2))
-    command_queue.write(move_to_command)
-
-    # эта команда должна уйти в command_queue_2
-    noop_command = NoOpCommand(None)
-    command_queue.write(noop_command)
     assert command_queue_2.get(True, 1.0) is noop_command
 
     # переключение в режим Run
@@ -64,3 +48,9 @@ def test_run_command():
     # соответственно, не должна дойти до второй очереди команд
     with pytest.raises(Empty):
         command_queue_2.get(True, 1.0)
+
+    # теперь надо завершить аттрактор, иначе тест не считается завершенным
+    stop_command = StopAttractorCommand(attractor)
+    command_queue.write(stop_command)
+    attractor.join(1.0)
+
